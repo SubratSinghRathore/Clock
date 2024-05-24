99540509d53cbfde6bdca7090a2ced56ae2b25f2
SubratSinghRathore <subratsingh777@gmail.com>
2024-05-25 01:03:15 +0530

    project completed

--- app.js
+++ app.js
@@ -84,6 +84,20 @@ switch (day) {
 document.getElementById('day').innerHTML = stringDay;
 
 
+d = new Date()
+htime = d.getHours()
+mtime = d.getMinutes()
+stime = d.getSeconds()
+hrotation = 30*htime + mtime/2
+mrotation = 6*mtime
+srotation = 6*stime
+
+hour.style.transform = `rotate(${hrotation}deg)`
+minute.style.transform = `rotate(${mrotation}deg)`
+second.style.transform = `rotate(${srotation}deg)`
+
+
+
 setInterval(() => {
     const currentTime = new Date();
 
@@ -171,6 +185,20 @@ setInterval(() => {
             break;
     }
     document.getElementById('day').innerHTML = stringDay;
+
+
+
+    d = new Date()
+    htime = d.getHours()
+    mtime = d.getMinutes()
+    stime = d.getSeconds()
+    hrotation = 30*htime + mtime/2
+    mrotation = 6*mtime
+    srotation = 6*stime
+    
+    hour.style.transform = `rotate(${hrotation}deg)`
+    minute.style.transform = `rotate(${mrotation}deg)`
+    second.style.transform = `rotate(${srotation}deg)`
 
 }, 1000);
 
